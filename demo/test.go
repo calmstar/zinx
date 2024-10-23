bc56e6dde4fa3aa53e72bb5a12ce40a16fe3b989
er <er.com>
2024-10-23 17:27:00 +0800

    connection

--- demo/test.go
+++ demo/test.go
@@ -3,46 +3,82 @@ package main
 import "fmt"
 
 func main() {
-	adapter := newAdapter(v220{})
-	p := &phone{v5: adapter}
-	p.charging()
+	s1 := &student{name: "zhangsan"}
+	s2 := &student{name: "lisi"}
+	s3 := &student{name: "wangwu"}
+
+	fmt.Println(s1.name, s2.name, " 加入观察班长信号队列中")
+	observerList := []observer{s1, s2}
+	m := &monitor{observerList}
+	fmt.Println(s3.name, " 加入观察班长信号队列中")
+	m.add(s3)
+
+	// 老师来了
+	fmt.Println("老师来了")
+	m.notify()
+
+	fmt.Println("========")
+	fmt.Println("踢出观察者：", s2.name)
+	m.del(s2)
+	fmt.Println("老师又来了")
+	m.notify()
 }
 
-type v5 interface {
-	use5v()
+/**
+触发：教师来了，
+被观察者：班长，站岗看老师是否来了
+观察者：学生，观察班长是否通知他们了
+*/
+
+// 抽象层 ----------
+type observer interface {
+	doSomething()
 }
 
-// -------
-type phone struct {
-	v5 v5
+type beObserver interface {
+	add(o observer)
+	del(o observer)
+	notify()
 }
 
-func newPhone(v5 v5) *phone {
-	return &phone{v5: v5}
-}
-func (p *phone) charging() {
-	p.v5.use5v()
-}
-
-// -------
-type adapter struct {
-	v220 v220
+// 基础层----------
+// 班长：充当被观察者（被学生观察的人）
+type monitor struct {
+	oList []observer
 }
 
-func newAdapter(v220 v220) *adapter {
-	return &adapter{v220: v220}
+func (m *monitor) notify() {
+	if m.oList == nil {
+		return
+	}
+
+	for _, o := range m.oList {
+		o.doSomething()
+	}
 }
 
-func (a *adapter) use5v() {
-	// 转换操作
-	fmt.Println("开始执行转换操作，将220v转成5v")
-	a.v220.Use220v()
+func (m *monitor) add(o observer) {
+	m.oList = append(m.oList, o)
 }
 
-// -------
-type v220 struct {
+func (m *monitor) del(o observer) {
+	needDelIdx := -1
+	for i, v := range m.oList {
+		if v == o {
+			needDelIdx = i
+			break
+		}
+	}
+	if needDelIdx != -1 {
+		m.oList = append(m.oList[:needDelIdx], m.oList[needDelIdx+1:]...)
+	}
 }
 
-func (v *v220) Use220v() {
-	fmt.Println("使用220v")
+// 学生：充当观察者，观察班长给出的通知信号，然后做点动作
+type student struct {
+	name string
+}
+
+func (s *student) doSomething() {
+	fmt.Println(s.name, ", 停止嬉皮笑脸")
 }
